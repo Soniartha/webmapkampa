3b8b0649b473d66f876f5d9eb775839cc715229e
Soniartha <65614408+Soniartha@users.noreply.github.com>
2021-07-12 17:04:01 +0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1U4CvI1wt-25r243GWBGI0KQFUL2fWsjCZUgtTZTMojY/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-// var googleApiKey = '101574488701-0vclf49t63hupjab2j7ljv518ala5foj.apps.googleusercontent.com';
+var googleApiKey = 'AIzaSyCU1BMQBqmhDRTdDw2Dv9I9NGTZ7omL6ag';
